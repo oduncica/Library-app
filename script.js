1eb844facc2015a2caba84527d6b3a0b5a71ba4d
oduncica <oduncica@gmail.com>
2024-02-23 11:52:40 +0100

    add book to library function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+//constructeur
+
 function Book(title, author, pages, isRead) {
   this.title= title;
   this.author = author;
@@ -8,30 +10,54 @@ function Book(title, author, pages, isRead) {
   this.info = function () {
     return "The " + this.title + " by " + this.author + this.pages + this.isRead;
   }     
-
-
 }
-
 
 // prendre les entrées de l'utilisateur et stocker les nouveaux livres dans un tableau
-const addBook = document.querySelector('#addBook');
+const addBtn = document.querySelector('#addBook');
+const form = document.querySelector('#form')
+const div = document.querySelector('.div')
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const isRead = document.querySelector('#isRead');
+const submit = document.querySelector('#submit');
+
+form.addEventListener('submit',   (event) => event.preventDefault()
+
+)
+
+
 function addBookToLibrary() {
-    let book = prompt('what book do you want to add ?');
-    if (book  != "") {
-        myLibrary.push(book);
-        book = "";
-        console.log(myLibrary)
+  const newBook = {
+      title: title.value,
+      author: author.value,
+      pages: pages.value,
+      isRead: isRead.value
     }
+    myLibrary.push(newBook)
+    console.log(myLibrary)
+
+    function printBook () {
+      div.innerHTML = `<ul>`
+  for (let index = 0; index < myLibrary.length; index++) {
+
+    let element = document.createElement('li')
+
+    //  newBook = myLibrary[index];
+
+    element.textContent = `${newBook.title} is the title ${newBook.author} is the author name,  ${newBook.pages} is the author name and ${newBook.isRead} is the read state`
+    div.appendChild(element)
+  }
+     div.innerHTML += `</ul>` 
 }
+printBook();      
+  }
 
-addBook.addEventListener('click', addBookToLibrary())
+  
 
-function printBook () {
-    myLibrary.forEach(element => {
-        
-    });
+ 
+submit.addEventListener('click', addBookToLibrary)
 
-}
-
+    // div.textContent= `${newBook.title} is the title ${newBook.author} is the author name,  ${newBook.pages} is the author name and ${newBook.isRead} is the read state`
 
 
