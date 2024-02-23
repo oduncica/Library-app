1eafad8d3d858c385e7c6e73e8e069a40c2fce2b
oduncica <oduncica@gmail.com>
2024-02-23 20:32:32 +0100

    add view to prin book function

--- script.js
+++ script.js
@@ -1,63 +1,79 @@
 const myLibrary = [];
 
 //constructeur
-
-function Book(title, author, pages, isRead) {
-  this.title= title;
+function Book(identifiant,title, author, pages, isRead) {
+  this.identifiant = identifiant;
+  this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
   this.info = function () {
-    return "The " + this.title + " by " + this.author + this.pages + this.isRead;
-  }     
+    return (
+      "The " + this.title + " by " + this.author + this.pages + this.isRead
+    );
+  };
 }
 
 // prendre les entrées de l'utilisateur et stocker les nouveaux livres dans un tableau
-const addBtn = document.querySelector('#addBook');
-const form = document.querySelector('#form')
-const div = document.querySelector('.div')
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const pages = document.querySelector('#pages');
-const isRead = document.querySelector('#isRead');
-const submit = document.querySelector('#submit');
+const addBtn = document.querySelector("#addBtn");
+const form = document.querySelector("#form");
+const div = document.querySelector(".div");
+const identifiant = document.querySelector("#identifiant");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const isRead = document.querySelector("#isRead");
+const submit = document.querySelector("#submit");
+const deleteBtn = document.querySelector("#delete");
 
-form.addEventListener('submit',   (event) => event.preventDefault()
 
-)
-
+form.addEventListener("submit", (event) => event.preventDefault());
 
 function addBookToLibrary() {
   const newBook = {
-      title: title.value,
-      author: author.value,
-      pages: pages.value,
-      isRead: isRead.value
-    }
-    myLibrary.push(newBook)
-    console.log(myLibrary)
+    identifiant : identifiant.value,
+    title: title.value,
+    author: author.value,
+    pages: pages.value,
+    isRead: isRead.value,
+  };
+  myLibrary.push(newBook);
+  console.log(myLibrary);
 
-    function printBook () {
-      div.innerHTML = `<ul>`
-  for (let index = 0; index < myLibrary.length; index++) {
 
-    let element = document.createElement('li')
-
-    //  newBook = myLibrary[index];
-
-    element.textContent = `${newBook.title} is the title ${newBook.author} is the author name,  ${newBook.pages} is the author name and ${newBook.isRead} is the read state`
-    div.appendChild(element)
-  }
-     div.innerHTML += `</ul>` 
+  printBook();
 }
-printBook();      
+
+function printBook() {
+  // div.innerHTML = `<ul>`;
+  for (let index = 0; index < myLibrary.length; index++) {
+    let container = document.createElement("div");
+    container.classList.add('container')
+
+    let actualBook = myLibrary[index];
+
+    container.innerHTML = 
+    `<p> <em> Identifiant:  </em> ${actualBook.identifiant}</p>
+    <p> <em> Title: </em>  ${actualBook.title}</p>
+    <p>  <em> Author:</em> ${actualBook.author}</p>
+    <p>  <em>Pages: </em> ${actualBook.pages}</p>
+    <p>  <em> Read state: </em> ${actualBook.isRead}</p>`
+    div.appendChild(container);
   }
+  // div.innerHTML += `</ul>`;
+}
 
-  
+submit.addEventListener("click", addBookToLibrary);
 
- 
-submit.addEventListener('click', addBookToLibrary)
+addBtn.addEventListener("click", function () {
+  form.style.display = "block";
+});
 
-    // div.textContent= `${newBook.title} is the title ${newBook.author} is the author name,  ${newBook.pages} is the author name and ${newBook.isRead} is the read state`
+deleteBtn.addEventListener("click", function () {
 
+  myLibrary.splice(1,identifiant)
+  div.innerHTML= '',
+   printBook();
+});
 
+// div.textContent= `${newBook.title} is the title ${newBook.author} is the author name,  ${newBook.pages} is the author name and ${newBook.isRead} is the read state`
